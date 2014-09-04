d625168a96a9c7f49c4c7efcc29fec04f93fa097
stal <stal@zodiaclabs.org>
2014-09-04 10:16:23 -0700

    png.c

--- Formula/utox.rb
+++ Formula/utox.rb
@@ -8,11 +8,6 @@ class Utox < Formula
   depends_on "freealut"
 
   def install
-    system("curl", "-o", "lodepng.c",
-           "https://lpi.googlecode.com/svn/trunk/lodepng.cpp")
-    system("curl", "-o", "lodepng.h",
-           "https://lpi.googlecode.com/svn/trunk/lodepng.h")
-
     includes = ["-I/usr/local/include", "-I/usr/X11/include",
                 "-I/usr/X11/include/freetype2"]
     libdirs  = ["-L/usr/local/lib", "-L/usr/X11/lib"]
@@ -21,7 +16,7 @@ class Utox < Formula
             "-lXft", "-lfontconfig", "-lfreetype", "-lvpx"]
     Dir.mkdir("#{prefix}/bin", 0775)
     system("cc", "-o", "#{prefix}/bin/utox", *includes, *libdirs, *libs,
-           *Dir.glob("*.c"))
+           *Dir.glob("*.c"), "png/png.c")
   end
 
 end
