b7381d6905583524ea843ea60332cd4a96dc8bac
stal <stal@zodiaclabs.org>
2015-12-21 19:52:30 -0800

    fix utox build command

--- Formula/utox.rb
+++ Formula/utox.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Utox < Formula
-  head "git://github.com/GrayHatter/uTox", :using => :git
+  head "git://github.com/GrayHatter/uTox", :using => :git, :branch => "develop"
   homepage "https://tox.chat"
 
   depends_on "libtoxcore"
@@ -9,7 +9,7 @@ class Utox < Formula
   depends_on "libfilteraudio"
 
   def install
-    system "make", "-f", "cocoa/Makefile", "uTox.app"
+    system "make", "-f", "src/cocoa/Makefile", "uTox.app"
     prefix.install "uTox.app"
   end
 end
