c23a8ecfc909eb983f1dc3c085190a052abb2f9e
Steve Leonard <xsleonard@gmail.com>
2017-04-23 14:55:28 -0400

    Update repo urls, fix some deps

--- Formula/utox.rb
+++ Formula/utox.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class Utox < Formula
-  head "git://github.com/GrayHatter/uTox", :using => :git, :branch => "develop"
+  head "git://github.com/uTox/uTox", :using => :git, :branch => "develop"
   homepage "https://tox.chat"
 
-  depends_on "libtoxcore"
   depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+  depends_on "libtoxcore"
   depends_on "libfilteraudio"
 
   def install
