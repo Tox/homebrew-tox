981d21a864b0c818b3f423430782f6a16be46270
William Nemencha <contact@wnemencha.me>
2022-05-06 20:00:26 +0200

    Update libtoxcore@0.1.rb

--- Formula/libtoxcore@0.1.rb
+++ Formula/libtoxcore@0.1.rb
@@ -37,7 +37,10 @@ class LibtoxcoreAT01 < Formula
 
     ENV["LDFLAGS"] = "-mmacosx-version-min=10.6"
     ENV["CFLAGS"] = "-mmacosx-version-min=10.6"
-    system "cmake", ".", *args, *std_cmake_args
+    
+    system "mkdir _build"
+    system "cd _build"
+    system "cmake", "..", *args, *std_cmake_args
     system "make"
     system "make", "install"
   end
