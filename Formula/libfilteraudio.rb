5f1645ef866c710e9c969f6d5f430ba6c3c82d4d
William Nemencha <contact@wnemencha.me>
2022-05-06 19:45:26 +0200

    Update libfilteraudio.rb
    git:// to https:// (c.f. https://github.blog/2021-09-01-improving-git-protocol-security-github/)

--- Formula/libfilteraudio.rb
+++ Formula/libfilteraudio.rb
@@ -3,7 +3,7 @@ class Libfilteraudio < Formula
   homepage "https://github.com/irungentoo/filter_audio"
   url "https://github.com/irungentoo/filter_audio/archive/v0.0.1.tar.gz"
   sha256 "fb135592c5133c3b4b664da18f988f58609db912f204059abe16277df044a366"
-  head "git://github.com/irungentoo/filter_audio"
+  head "https://github.com/irungentoo/filter_audio"
 
   def install
     ENV["CFLAGS"] = "-mmacosx-version-min=10.6"
